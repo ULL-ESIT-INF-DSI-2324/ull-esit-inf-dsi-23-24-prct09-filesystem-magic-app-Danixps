cc849c2ee50de39c145ff06e61614ddd84246eb9
Daniel Exposito <alu0101481876@ull.edu.es>
2024-03-24 18:23:31 +0000

    Realizando los tests

--- src/index.ts
+++ src/index.ts
@@ -91,7 +91,7 @@ yargs(process.argv.slice(2))
             },
         },
         handler: (argv) => {
-            
+
             const carta = new Card(
                 argv.id,
                 argv.name,
@@ -272,3 +272,27 @@ yargs(process.argv.slice(2))
     })
     .help()
     .argv;
+
+export function mostrarCartas(user: string): string {
+    const cartas = cargarCartas(user);
+    let cardInfo = '';
+
+    cardInfo+= `${user} collection\n`;
+    cardInfo+='--------------------------------';
+    cartas.forEach((carta) => {
+        
+        cardInfo += `\nID: ${carta.id}\nName: ${carta.name}\nManaCost: ${carta.manaCost}\nColor: ${carta.color}\nType: ${carta.type}\nRarity: ${carta.rarity}\nRulesText: ${carta.rulesText}`;
+
+        if (carta.powerandtoughness !== undefined) {
+            cardInfo += `\nPower/Toughness: ${carta.powerandtoughness}`;
+        }
+
+        if (carta.loyalty !== undefined) {
+            cardInfo += `\nLoyalty: ${carta.loyalty}`;
+        }
+
+        cardInfo += `\nMarketValue: ${carta.marketValue}\n--------------------------------`;
+    });
+
+    return cardInfo;
+}
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import { execSync } from 'child_process';
 import { existsSync, readFileSync } from 'fs';
 import { Card, Color, LineType, Rarity } from '../src/card.js';
-
+import { mostrarCartas } from '../src/index.js';
 describe('Aplicación Magic Cards', () => {
 
     it('Añadir una carta. Intento de añadir con Fuerza/Resistencia en una línea de tipo que es distinta a Criatura', () => {
@@ -89,11 +89,8 @@ describe('Aplicación Magic Cards', () => {
         expect(card.guardarCarta('danixps')).to.be.equal('New card saved at danixps collection!'); //ahora si se añade por primera vez
     });
 
-    it ('Comando list', () => {
-        const output = execSync('node dist/index.js list --user "danixps"').toString();
-        expect(output).to.eql(
-        `danixps collection\n--------------------------------\nID: 1\nName: White Panter\nManaCost: 20\nColor: incoloro\nType: planeswalker\nRarity: mítica\nRulesText: Tap to atack the enemy and gain 5 life points.\nLoyalty: 10\nMarketValue: 1234\n--------------------------------\nID: 777\nName: Black Lotus\nManaCost: 69\nColor: multicolor\nType: criatura\nRarity: común\nRulesText: Tap to add three mana of any color to your mana pool.\nPower/Toughness: 5,11\nMarketValue: 1234\n--------------------------------\n`
-        );
+    it ('Listar las cartas del usuario danixps', () => {
+        expect(mostrarCartas('danixps')).to.be.equal('danixps collection\n--------------------------------\nID: 1\nName: White Panter\nManaCost: 20\nColor: incoloro\nType: planeswalker\nRarity: mítica\nRulesText: Tap to atack the enemy and gain 5 life points.\nLoyalty: 10\nMarketValue: 1234\n--------------------------------\nID: 777\nName: Black Lotus\nManaCost: 69\nColor: multicolor\nType: criatura\nRarity: común\nRulesText: Tap to add three mana of any color to your mana pool.\nPower/Toughness: 5,11\nMarketValue: 1234\n--------------------------------');
     });
 
     // it ('Comando add con una nueva carta', () => {
